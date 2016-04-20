770bcc96128ba068c022c482c6066013816d67be
Stephen Bresnick <stevebresnick@gmail.com>
2016-04-20 12:05:18 -0400

    Hello Block plugin

---
+++ hello_world/HelloBlock.php
@@ -0,0 +1,23 @@
+/**
+ * Provides a 'Hello' Block
+ *
+ * @Block(
+ *   id = "hello_block",
+ *   admin_label = @Translation("Hello block"),
+ * )
+ */
+
+namespace Drupal\hello_world\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+class HelloBlock extends BlockBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return array(
+      '#markup' => $this->t('Hello, World!'),
+    );
+  }
+}
